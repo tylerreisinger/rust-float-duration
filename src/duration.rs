51686cb524ec4934978a4e2ff920e6bf7160e83a
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-05-24 12:02:34 -0400

    Added a few more arithmetic operator overloads for `FloatDuration`.
    Wrote some new tests.

--- src/duration.rs
+++ src/duration.rs
@@ -12,29 +12,29 @@ use super::error::DurationError;
 /// Number of seconds in a minute.
 pub const SECS_PER_MINUTE: f64 = 60.0;
 /// Number of seconds in an hour.
-pub const SECS_PER_HOUR: f64 = SECS_PER_MINUTE*60.0;
+pub const SECS_PER_HOUR: f64 = SECS_PER_MINUTE * 60.0;
 /// Number of seconds in a day.
-pub const SECS_PER_DAY: f64 = SECS_PER_HOUR*24.0;
+pub const SECS_PER_DAY: f64 = SECS_PER_HOUR * 24.0;
 /// Number of seconds in a year.
-pub const SECS_PER_YEAR: f64 = SECS_PER_DAY*365.0;
+pub const SECS_PER_YEAR: f64 = SECS_PER_DAY * 365.0;
 
 
 /// A specific point in time.
-/// 
+///
 /// Types implementing `TimePoint` can have a `FloatDuration` computed between them
 /// via `float_duration_since` in either direction.
-pub trait TimePoint<Rhs=Self> {
+pub trait TimePoint<Rhs = Self> {
     /// The amount of time between two `TimePoint`s.
     fn float_duration_since(self, rhs: Rhs) -> error::Result<FloatDuration>;
 }
 
 /// A time duration stored as a floating point quantity.
-/// 
+///
 /// Unlike `std::time::Duration` or `chrono::Duration`, `FloatDuration`
-/// aims to be convenient and fast to use in simulation and mathematical expressions 
+/// aims to be convenient and fast to use in simulation and mathematical expressions
 /// rather than to behave like a calendar or perfectly
-/// accurately represent precise time scales. 
-/// 
+/// accurately represent precise time scales.
+///
 /// Internally, a `FloatDuration` stores a single `f64` number of floating-point seconds,
 /// thus it is only as precise as the `f64` type.
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
@@ -43,7 +43,7 @@ pub struct FloatDuration {
 }
 
 /// A duration decomposed into components.
-/// 
+///
 /// `DecomposedTime` is mainly provided for a more human-readable and composable
 /// representation of a `FloatDuration`. It may be converted back-and-forth
 /// between `FloatDuration` at will.
@@ -60,55 +60,55 @@ pub struct DecomposedTime {
 impl FloatDuration {
     /// Create a new `FloatDuration` representing a number of days.
     pub fn days(days: f64) -> FloatDuration {
-        FloatDuration {secs: days * (3600.0 * 24.0)}
+        FloatDuration { secs: days * (3600.0 * 24.0) }
     }
     /// Create a new `FloatDuration` representing a number of hours.
     pub fn hours(hours: f64) -> FloatDuration {
-        FloatDuration {secs: hours * 3600.0}
+        FloatDuration { secs: hours * 3600.0 }
     }
     /// Create a new `FloatDuration` representing a number of minutes.
     pub fn minutes(mins: f64) -> FloatDuration {
-        FloatDuration {secs: mins * 60.0}
+        FloatDuration { secs: mins * 60.0 }
     }
     /// Create a new `FloatDuration` representing a number of seconds.
     pub fn seconds(secs: f64) -> FloatDuration {
-        FloatDuration {secs}
+        FloatDuration { secs }
     }
     /// Create a new `FloatDuration` representing a number of milliseconds.
     pub fn milliseconds(millis: f64) -> FloatDuration {
-        FloatDuration {secs: millis / 1000.0}
+        FloatDuration { secs: millis / 1000.0 }
     }
     /// Create a new `FloatDuration` representing a number of microseconds.
     pub fn microseconds(micros: f64) -> FloatDuration {
-        FloatDuration {secs: micros / 1.0e6}
+        FloatDuration { secs: micros / 1.0e6 }
     }
     /// Create a new `FloatDuration` representing a number of nanoseconds.
     pub fn nanoseconds(nanos: f64) -> FloatDuration {
-        FloatDuration {secs: nanos / 1.0e9}
+        FloatDuration { secs: nanos / 1.0e9 }
     }
 
     /// Decompose a `FloatDuration` into a `DecomposedTime` representation.
-    /// 
+    ///
     /// The result represents the same duration, but in a more human-readable
-    /// way. It may be converted back into a `FloatDuration` via 
+    /// way. It may be converted back into a `FloatDuration` via
     /// `FloatDuration::from_decomposed`.
     pub fn decompose(&self) -> DecomposedTime {
         let mut secs_left = self.secs;
 
         let days = (secs_left / SECS_PER_DAY).trunc();
-        secs_left -= days*SECS_PER_DAY;
+        secs_left -= days * SECS_PER_DAY;
         let hours = (secs_left / SECS_PER_HOUR).trunc();
-        secs_left -= hours*SECS_PER_HOUR;
+        secs_left -= hours * SECS_PER_HOUR;
         let minutes = (secs_left / SECS_PER_MINUTE).trunc();
-        secs_left -= minutes*SECS_PER_MINUTE;
+        secs_left -= minutes * SECS_PER_MINUTE;
         let seconds = secs_left.trunc();
         let fract = secs_left.fract();
 
         DecomposedTime {
             days: days as u32,
             hours: hours as u32,
-            minutes: minutes as u32, 
-            seconds: seconds as u32, 
+            minutes: minutes as u32,
+            seconds: seconds as u32,
             fractional_seconds: fract,
             sign: self.secs.signum() as i8,
         }
@@ -149,11 +149,11 @@ impl FloatDuration {
     }
     /// Return a new `FloatDuration` that represents zero elapsed time.
     pub fn zero() -> FloatDuration {
-        FloatDuration {secs: 0.0}
+        FloatDuration { secs: 0.0 }
     }
     /// Returns true is this duration represents zero elapsed time (equals `FloatDuration::zero()`).
     pub fn is_zero(&self) -> bool {
-        self.secs == 0.0 
+        self.secs == 0.0
     }
     /// Returns true if the FloatDuration holds a positive amount of time.
     pub fn is_positive(&self) -> bool {
@@ -174,10 +174,10 @@ impl FloatDuration {
     }
 
     /// Create a `std::time::Duration` object from a `FloatDuration`.
-    /// 
+    ///
     /// # Errors
     /// `std::time::Duration` does not support negative values or seconds
-    /// greater than `std::u64::MAX`. This function will return a 
+    /// greater than `std::u64::MAX`. This function will return a
     /// `DurationError::StdOutOfRange` if the `FloatDuration` value is outside
     /// of either of those bounds.
     pub fn as_std(&self) -> error::Result<time::Duration> {
@@ -197,21 +197,18 @@ impl FloatDuration {
 
     /// Create a `FloatDuration` object from a `std::time::Duration`.
     pub fn from_std(duration: time::Duration) -> FloatDuration {
-        FloatDuration::seconds(
-            (duration.as_secs() as f64) + (duration.subsec_nanos() as f64)*1.0e-9)
+        FloatDuration::seconds((duration.as_secs() as f64) +
+                               (duration.subsec_nanos() as f64) * 1.0e-9)
     }
 
     /// Create a `FloatDuration` object from a `DecomposedTime`.
     pub fn from_decomposed(decomposed: &DecomposedTime) -> FloatDuration {
-        let seconds = (decomposed.days as f64)*SECS_PER_DAY
-            + (decomposed.hours as f64)*SECS_PER_HOUR
-            + (decomposed.minutes as f64)*SECS_PER_MINUTE
-            + (decomposed.seconds as f64)
-            + decomposed.fractional_seconds;
+        let seconds =
+            (decomposed.days as f64) * SECS_PER_DAY + (decomposed.hours as f64) * SECS_PER_HOUR +
+            (decomposed.minutes as f64) * SECS_PER_MINUTE +
+            (decomposed.seconds as f64) + decomposed.fractional_seconds;
 
-        FloatDuration {
-            secs: (decomposed.sign as f64)*seconds
-        }
+        FloatDuration { secs: (decomposed.sign as f64) * seconds }
     }
 }
 
@@ -226,18 +223,15 @@ impl FloatDuration {
             Ok(chrono_duration)
         }
     }
-    
-    pub fn from_chrono_duration(duration: &chrono::Duration) 
-            -> error::Result<FloatDuration> 
-    {
-        let is_negative = duration.num_seconds() < 0;        
 
-        let std_duration =
-            if is_negative {
-                (-*duration).to_std()?
-            } else {
-                duration.to_std()?
-            };
+    pub fn from_chrono_duration(duration: &chrono::Duration) -> error::Result<FloatDuration> {
+        let is_negative = duration.num_seconds() < 0;
+
+        let std_duration = if is_negative {
+            (-*duration).to_std()?
+        } else {
+            duration.to_std()?
+        };
 
         let float_duration = FloatDuration::from_std(std_duration);
         if is_negative {
@@ -250,15 +244,30 @@ impl FloatDuration {
 
 impl DecomposedTime {
     pub fn zero() -> DecomposedTime {
-        DecomposedTime { days: 0, hours: 0, minutes: 0, seconds: 0,
-            fractional_seconds: 0.0, sign: 1 }
+        DecomposedTime {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+            fractional_seconds: 0.0,
+            sign: 1,
+        }
     }
 
-    pub fn from_components(days: u32, hours: u32, minutes: u32, 
-               seconds: u32, fractional_seconds: f64) -> DecomposedTime 
-    {
+    pub fn from_components(days: u32,
+                           hours: u32,
+                           minutes: u32,
+                           seconds: u32,
+                           fractional_seconds: f64)
+                           -> DecomposedTime {
         DecomposedTime {
-            days, hours, minutes, seconds, fractional_seconds, sign: 1}
+            days,
+            hours,
+            minutes,
+            seconds,
+            fractional_seconds,
+            sign: 1,
+        }
     }
 
     pub fn negate(mut self) -> DecomposedTime {
@@ -268,9 +277,7 @@ impl DecomposedTime {
 }
 
 impl<Tz: chrono::TimeZone> TimePoint for chrono::DateTime<Tz> {
-    fn float_duration_since(self, since: chrono::DateTime<Tz>) 
-            -> error::Result<FloatDuration> 
-    {
+    fn float_duration_since(self, since: chrono::DateTime<Tz>) -> error::Result<FloatDuration> {
         let chrono_duration = self.signed_duration_since(since);
         FloatDuration::from_chrono_duration(&chrono_duration)
     }
@@ -297,11 +304,12 @@ impl fmt::Display for DecomposedTime {
         if self.sign.is_negative() {
             write!(fmt, "-")?;
         }
-        write!(fmt, "{:02}:{:02}:{:02}.{}", 
-            self.hours,
-            self.minutes,
-            self.seconds,
-            self.fractional_seconds)
+        write!(fmt,
+               "{:02}:{:02}:{:02}.{}",
+               self.hours,
+               self.minutes,
+               self.seconds,
+               self.fractional_seconds)
     }
 }
 
@@ -329,7 +337,7 @@ impl ops::Neg for FloatDuration {
     type Output = FloatDuration;
 
     fn neg(self) -> FloatDuration {
-        FloatDuration {secs: -self.secs}
+        FloatDuration { secs: -self.secs }
     }
 }
 
@@ -337,14 +345,14 @@ impl ops::Add<FloatDuration> for FloatDuration {
     type Output = FloatDuration;
 
     fn add(self, rhs: FloatDuration) -> FloatDuration {
-        FloatDuration {secs: self.secs + rhs.secs}
+        FloatDuration { secs: self.secs + rhs.secs }
     }
 }
 impl ops::Sub<FloatDuration> for FloatDuration {
     type Output = FloatDuration;
 
     fn sub(self, rhs: FloatDuration) -> FloatDuration {
-        FloatDuration {secs: self.secs - rhs.secs}
+        FloatDuration { secs: self.secs - rhs.secs }
     }
 }
 
@@ -352,14 +360,28 @@ impl ops::Mul<f64> for FloatDuration {
     type Output = FloatDuration;
 
     fn mul(self, rhs: f64) -> FloatDuration {
-        FloatDuration {secs: self.secs * rhs}
+        FloatDuration { secs: self.secs * rhs }
+    }
+}
+impl ops::Mul<FloatDuration> for f64 {
+    type Output = FloatDuration;
+
+    fn mul(self, rhs: FloatDuration) -> FloatDuration {
+        FloatDuration { secs: self * rhs.secs }
     }
 }
 impl ops::Div<f64> for FloatDuration {
     type Output = FloatDuration;
 
     fn div(self, rhs: f64) -> FloatDuration {
-        FloatDuration {secs: self.secs / rhs}
+        FloatDuration { secs: self.secs / rhs }
+    }
+}
+impl ops::Div<FloatDuration> for FloatDuration {
+    type Output = f64;
+
+    fn div(self, rhs: FloatDuration) -> f64 {
+        self.secs / rhs.secs
     }
 }
 
@@ -400,9 +422,10 @@ mod tests {
         let duration1 = FloatDuration::hours(3.0);
         assert_eq!(duration1.as_hours(), 3.0);
         assert_eq!(duration1.as_minutes(), 180.0);
-        assert_eq!(duration1.as_seconds(), 180.0*60.0);
-        assert_eq!(duration1.as_days(), 3.0/24.0);
-        assert_eq!(duration1.as_milliseconds(), 180.0*60.0*1000.0);
+        assert_eq!(duration1.as_seconds(), 180.0 * 60.0);
+        assert_eq!(duration1.as_days(), 3.0 / 24.0);
+        assert_eq!(duration1.as_milliseconds(), 180.0 * 60.0 * 1000.0);
+        assert!(duration1.is_positive());
 
         let duration2 = FloatDuration::milliseconds(55.0);
         assert_eq!(duration2.as_seconds(), 0.055);
@@ -413,30 +436,49 @@ mod tests {
 
         let duration3 = FloatDuration::zero();
         assert!(duration3.is_zero());
+        assert_eq!(duration3.as_minutes(), 0.0);
+        assert_eq!(duration3.as_nanoseconds(), 0.0);
 
         assert_eq!(FloatDuration::days(1.5), FloatDuration::hours(36.0));
         assert_eq!(FloatDuration::minutes(30.0), FloatDuration::hours(0.5));
         assert_eq!(FloatDuration::seconds(180.0), FloatDuration::minutes(3.0));
-        assert_eq!(FloatDuration::seconds(3.5), FloatDuration::milliseconds(3500.0));
-        assert_eq!(FloatDuration::microseconds(300.0), FloatDuration::milliseconds(0.30));
-        assert_eq!(FloatDuration::nanoseconds(1000.0), FloatDuration::microseconds(1.0));
+        assert_eq!(FloatDuration::seconds(3.5),
+                   FloatDuration::milliseconds(3500.0));
+        assert_eq!(FloatDuration::microseconds(300.0),
+                   FloatDuration::milliseconds(0.30));
+        assert_eq!(FloatDuration::nanoseconds(1000.0),
+                   FloatDuration::microseconds(1.0));
+
+        let duration4 = FloatDuration::minutes(-3.0);
+        assert_eq!(duration4.as_minutes(), -3.0);
+        assert_eq!(duration4.as_hours(), -0.05);
+        assert!(duration4.is_negative());
     }
 
     #[test]
     fn test_arithmetic() {
-        let duration1 = FloatDuration::minutes(5.0) + FloatDuration::seconds(30.0);
-        assert_eq!(duration1, FloatDuration::seconds(330.0));
+        assert_eq!(FloatDuration::minutes(5.0) + FloatDuration::seconds(30.0),
+                   FloatDuration::seconds(330.0));
 
-        let duration2 = FloatDuration::hours(3.0) * 2.5;
-        assert_eq!(duration2, FloatDuration::hours(7.5));
+        assert_eq!(FloatDuration::hours(3.0) * 2.5, FloatDuration::hours(7.5));
 
-        let duration3 = FloatDuration::days(3.0) / 3.0 - FloatDuration::hours(2.0);
-        assert_eq!(duration3, FloatDuration::hours(22.0));
+        assert_eq!(FloatDuration::days(3.0) / 3.0 - FloatDuration::hours(2.0),
+                   FloatDuration::hours(22.0));
 
-        let duration4 = FloatDuration::zero() + FloatDuration::milliseconds(500.0) 
-            + FloatDuration::microseconds(500.0);
-        assert_eq!(duration4, FloatDuration::microseconds(500500.0));
+        assert_eq!(FloatDuration::zero() + FloatDuration::milliseconds(500.0) +
+                   FloatDuration::microseconds(500.0),
+                   FloatDuration::microseconds(500500.0));
 
+        assert_eq!(2.0 * FloatDuration::milliseconds(150.0),
+                   FloatDuration::milliseconds(300.0));
+
+        assert_eq!(FloatDuration::minutes(10.0) / FloatDuration::seconds(60.0),
+                   10.0);
+        assert_eq!(FloatDuration::minutes(5.0),
+                   (-FloatDuration::minutes(5.0)) * -1.0);
+
+        assert_eq!(FloatDuration::seconds(10.0) - FloatDuration::minutes(1.0),
+                   FloatDuration::seconds(-50.0));
     }
 
     #[test]
@@ -445,14 +487,14 @@ mod tests {
         let std_duration1 = duration1.as_std().unwrap();
         assert!(duration1.is_positive());
         assert_eq!(std_duration1, time::Duration::new(300, 0));
-        assert_eq!(FloatDuration::from_std(std_duration1).unwrap(), duration1);
-        
+        assert_eq!(FloatDuration::from_std(std_duration1), duration1);
+
         let duration2 = FloatDuration::hours(-2.0);
         assert!(duration2.is_negative());
         assert!(!duration2.as_std().is_ok());
         let std_duration2 = (-duration2).as_std().unwrap();
-        assert_eq!(std_duration2, time::Duration::new(3600*2, 0));
-        assert_eq!(FloatDuration::from_std(std_duration2).unwrap(), -duration2);
+        assert_eq!(std_duration2, time::Duration::new(3600 * 2, 0));
+        assert_eq!(FloatDuration::from_std(std_duration2), -duration2);
     }
 
     #[test]
@@ -462,7 +504,7 @@ mod tests {
         let mut buffer1 = "".to_string();
         let mut buffer1_2 = "".to_string();
         let duration1 = FloatDuration::minutes(3.5);
-        
+
         write!(buffer1, "{}", duration1).unwrap();
         write!(buffer1_2, "{}", duration1.decompose()).unwrap();
         assert_eq!(buffer1, "3.5 minutes");
