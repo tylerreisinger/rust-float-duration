6d90a77f92243249d4555b992b8663c1c431bd25
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-05-25 15:59:47 -0400

    Removed constants from top-level module.

--- src/lib.rs
+++ src/lib.rs
@@ -133,5 +133,3 @@ pub mod duration;
 pub mod error;
 
 pub use duration::{FloatDuration, TimePoint};
-pub use duration::{NANOS_PER_SEC, MICROS_PER_SEC, MILLIS_PER_SEC, SECS_PER_MINUTE, SECS_PER_DAY,
-                   SECS_PER_YEAR};
