54195cc6d12d6d7ac7d17fdfac52f59918a124ee
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-05-25 14:39:39 -0400

    Moved from glob `pub use` to specific items.

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,6 @@ extern crate approx;
 pub mod duration;
 pub mod error;
 
-pub use duration::*;
+pub use duration::{FloatDuration, TimePoint};
+pub use duration::{NANOS_PER_SEC, MICROS_PER_SEC, MILLIS_PER_SEC, SECS_PER_MINUTE, SECS_PER_DAY,
+                   SECS_PER_YEAR};
